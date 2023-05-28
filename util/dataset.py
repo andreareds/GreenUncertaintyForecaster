706d7132d6dde7f164918d6b9064bf0172f90318
dcarraroHP <diego.carraro@insight-centre.org>
2023-05-28 13:10:41 +0100

    Added scaled Alibaba dataset

--- util/dataset.py
+++ util/dataset.py
@@ -36,7 +36,7 @@ class Dataset:
 
         # Input files
         self.data_file = self.name  # "res_task_e.csv"  # "req_win_a.csv"  # "winDataset.csv"  # None
-        self.data_path = './saved_data/'
+        self.data_path = '../saved_data/'
 
         # Train/test split
         self.train_split = train_split
