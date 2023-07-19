70216bfe787c962f8d3d93bbc2c704de1a62954b
dcarraroHP <diego.carraro@insight-centre.org>
2023-07-19 13:10:25 +0200

    Code cleaned and results included

--- res/scenarios/old_scenarios.py
+++ res/scenarios/old_scenarios.py
@@ -28,6 +28,338 @@ def load_results(folder: str) -> dict:
     return results
 
 
+def allocate_resources_2(w_demand: float,
+                         df_allocation: pd.DataFrame,
+                         ) -> float:
+    wd_rem = w_demand
+
+    w = np.array(df_allocation.w)
+    e = np.array(df_allocation.e)
+
+    index = len(e)-1
+    cost = 0
+    while wd_rem > 0:
+        # Otherwise we select the most efficient
+        wd_rem -= w[index]
+        cost += e[index]
+        index -= 1
+    return cost
+
+
+def allocate_resources_3(w_demand: float,
+                         n_gpus_on: float,
+                         n_gpus_off: float
+                         ) -> (float, float, float):
+
+    wd_rem = w_demand
+    w_on = AVG_GPU_SPECS[1]
+    reduced_w_on = w_on - w_on*0.2
+
+    n_on_needed = int(wd_rem/w_on) + 1
+
+    if n_on_needed <= n_gpus_on:  # you just turn off some GPUs you do not need
+        new_n_on = n_on_needed
+    else:  # you need to turn on some more GPUs
+        wd_rem -= n_gpus_on*w_on
+        additional = int(wd_rem/reduced_w_on) + 1
+        new_n_on = n_gpus_on + additional
+
+    new_n_off = AVG_GPU_SPECS[0] - new_n_on
+    cost = new_n_on*AVG_GPU_SPECS[2]
+    return cost, new_n_on, new_n_off
+
+
+def allocate_resources_4(w_demand: float,
+                         df_allocation: pd.DataFrame,
+                         i: int,
+                         ) -> (float, pd.DataFrame):
+    # print(f"{i}: {datetime.datetime.now()}")
+    wd_rem = w_demand
+
+    names = df_allocation.gpu_name
+    w = np.array(df_allocation.w)
+    rw = np.array(df_allocation.rw)
+    s = np.array(df_allocation.s)
+    e = np.array(df_allocation.e)
+
+    index = len(s)-1
+    cost = 0
+    while wd_rem > 0:
+        # Otherwise we select the most efficient
+        if index < 0:
+            print()
+        wd_rem -= rw[index]
+        cost += e[index]
+        s[index] = 0
+        index -= 1
+
+    # all the first gpus needs to be switched OFF
+    off = np.array(list(map(lambda x: 1, s[:index+1])))
+    s = np.append(off, s[index+1:])
+
+    # calculate new workload based on the status
+    rw = [v-0.2*i for v, i in zip(w, s)]
+
+    d = {"gpu_name": list(names), 'w': list(w), 'rw': list(rw), 'e': list(e), 's': list(s)}
+    df_gpus_status = pd.DataFrame(data=d)
+
+    # sort by efficiency
+    df_gpus_status['eff'] = df_gpus_status.rw / df_gpus_status.e
+    df_gpus_status = df_gpus_status.sort_values(by='eff')
+    df_gpus_status.reset_index(inplace=True)
+
+    return cost, df_gpus_status
+
+
+def allocate_resources_11(w_demand: float,
+                          df_allocation: pd.DataFrame,
+                          ) -> float:
+    wd_rem = w_demand
+
+    w = np.array(df_allocation.w)
+    e = np.array(df_allocation.e)
+
+    index = len(e)-1
+    cost = 0
+    while wd_rem > 0:
+        # Otherwise we select the most efficient
+        wd_rem -= w[index]
+        cost += e[index]
+        index -= 1
+    return cost
+
+
+def run_scenario_2(args, results: dict):
+    # build dataframe of GPUs
+    w = []  # stores workload that GPUs can provide
+    e = []  # stores energy consumption
+    g_name = []  # stores the name of the GPUs
+    for gpu_name in GPU_SPECS:
+        w_value = GPU_SPECS[gpu_name][1]
+        n_g = GPU_SPECS[gpu_name][0]
+        e_g = GPU_SPECS[gpu_name][2]
+        g_name.extend([gpu_name for i in range(n_g)])
+        w.extend([w_value for i in range(n_g)])
+        e.extend([e_g for i in range(n_g)])
+    d = {"gpu_name": g_name, 'w': w, 'e': e}
+    df_gpus_status = pd.DataFrame(data=d)
+
+    # efficiency of GPU = workload provided / energy required
+    df_gpus_status['eff'] = df_gpus_status.w / df_gpus_status.e
+    df_gpus_status = df_gpus_status.sort_values(by='eff')
+    df_gpus_status.reset_index(inplace=True)
+
+    model_column = []
+    energy_costs = []
+    qos_column = []
+    for qos in QOS_LEVELS:
+        # load results
+        if args.debug_mode:
+            pred_workloads = {"baseline_b": [0.7 for i in range(len(results["HBNN"]))]}
+        else:
+            pred_workloads = {"oracle": results["HBNN"]["true_gpu"].values,
+                              "naive": [43916358.2147171 - 10
+                                        for i in range(len(results["HBNN"]))],  # max possible workload
+                              "HBNN": results["HBNN"][f"ub_{qos}"].values,
+                              "MCD": results["MCD"][f"ub_{qos}"].values,
+                              "HBNN++": results["HBNN++"][f"ub_{qos}"].values,
+                              "LSTMQ": results["LSTMQ"][f"ub_{qos}"].values,
+                              "LSTM": results["LSTM"][f"ub_{qos}"].values,
+                              "PROPHET": results["PROPHET"][f"ub_{qos}"].values,
+                              }
+
+        print(f"{datetime.datetime.now()} -- SCENARIO {args.id_scenario} for QOS {qos}!")
+        scenario_2_costs = {}
+        for model in pred_workloads:
+            demands = pred_workloads[model]
+            tot_costs = [allocate_resources_2(demand, df_gpus_status) for i, demand in enumerate(demands)]
+            scenario_2_costs[model] = np.sum(tot_costs)
+            model_column.append(model)
+            energy_costs.append(scenario_2_costs[model])
+            qos_column.append(qos)
+            print(f"Cost for {model}: {scenario_2_costs[model]}")
+            print(f"{datetime.datetime.now()} -- Done with model {model}!")
+        print(f"{datetime.datetime.now()} -- END!")
+
+    d = {"qos": qos_column,
+         "model": model_column,
+         "energy_cost": energy_costs}
+
+    df_results = pd.DataFrame(data=d)
+    df_results.to_csv("scenario_2_results.csv", index=False)
+
+
+def run_scenario_3(args, results: dict):
+
+    model_column = []
+    energy_costs = []
+    qos_column = []
+    for qos in QOS_LEVELS:
+        # load results
+        if args.debug_mode:
+            pred_workloads = {"oracle": results["HBNN"]["true_gpu"].values}
+        else:
+            pred_workloads = {"oracle": results["HBNN"]["true_gpu"].values,
+                              "naive": [43916358.2147171 - 10
+                                        for i in range(len(results["HBNN"]))],  # max possible workload
+                              "HBNN": results["HBNN"][f"ub_{qos}"].values,
+                              "MCD": results["MCD"][f"ub_{qos}"].values,
+                              "HBNN++": results["HBNN++"][f"ub_{qos}"].values,
+                              "LSTMQ": results["LSTMQ"][f"ub_{qos}"].values,
+                              "LSTM": results["LSTM"][f"ub_{qos}"].values,
+                              "PROPHET": results["PROPHET"][f"ub_{qos}"].values,
+                              }
+
+        print(f"{datetime.datetime.now()} -- SCENARIO {args.id_scenario} for QOS {qos}!")
+        scenario_3_costs = {}
+        for model in pred_workloads:
+            demands = pred_workloads[model]
+            tot_costs = 0
+            n_gpus_on = AVG_GPU_SPECS[0]
+            n_gpus_off = 0
+            for i, demand in enumerate(demands):
+                cost_i, n_gpus_on, n_gpus_off = allocate_resources_3(demand, n_gpus_on, n_gpus_off)
+                tot_costs += cost_i
+            scenario_3_costs[model] = np.sum(tot_costs)
+            model_column.append(model)
+            energy_costs.append(scenario_3_costs[model])
+            qos_column.append(qos)
+            print(f"Cost for {model}: {scenario_3_costs[model]}")
+            print(f"{datetime.datetime.now()} -- Done with model {model}!")
+        print(f"{datetime.datetime.now()} -- END!")
+        d = {"qos": qos_column,
+             "model": model_column,
+             "energy_cost": energy_costs}
+
+    d = {"qos": qos_column,
+         "model": model_column,
+         "energy_cost": energy_costs}
+    df_results = pd.DataFrame(data=d)
+    df_results.to_csv("scenario_3_results.csv", index=False)
+
+
+def run_scenario_4(args, results: dict):
+    # build dataframe of GPUs
+    w = []  # stores the max workload that GPUs can provide if ON at the beginning of the time window
+    rw = []  # stores workload that GPUs can provide based on their status
+    e = []  # stores energy consumption
+    s = []  # stores the status of GPUs (0=ON, 1=OFF)
+    g_name = []  # stores the name of the GPUs
+    for gpu_name in GPU_SPECS:
+        w_value = GPU_SPECS[gpu_name][1]
+        n_g = GPU_SPECS[gpu_name][0]
+        e_g = GPU_SPECS[gpu_name][2]
+        g_name.extend([gpu_name for i in range(n_g)])
+        w.extend([w_value for i in range(n_g)])
+        rw.extend([w_value for i in range(n_g)])
+        e.extend([e_g for i in range(n_g)])
+        s.extend([0 for i in range(n_g)])
+    d = {"gpu_name": g_name, 'w': w, 'rw': rw, 'e': e, 's': s}
+    df_gpus_status = pd.DataFrame(data=d)
+
+    # efficiency of GPU = workload provided / energy required
+    df_gpus_status['eff'] = df_gpus_status.rw / df_gpus_status.e
+    df_gpus_status = df_gpus_status.sort_values(by='eff')
+    df_gpus_status.reset_index(inplace=True)
+
+    model_column = []
+    energy_costs = []
+    qos_column = []
+    for qos in QOS_LEVELS:
+        # load results
+        if args.debug_mode:
+            pred_workloads = {"oracle": results["HBNN"]["true_gpu"].values}
+        else:
+            pred_workloads = {"oracle": results["HBNN"]["true_gpu"].values,
+                              "naive": [43916358.2147171 - 10
+                                        for i in range(len(results["HBNN"]))],  # max possible workload
+                              "HBNN": results["HBNN"][f"ub_{qos}"].values,
+                              "MCD": results["MCD"][f"ub_{qos}"].values,
+                              "HBNN++": results["HBNN++"][f"ub_{qos}"].values,
+                              "LSTMQ": results["LSTMQ"][f"ub_{qos}"].values,
+                              "LSTM": results["LSTM"][f"ub_{qos}"].values,
+                              "PROPHET": results["PROPHET"][f"ub_{qos}"].values,
+                              }
+
+        print(f"{datetime.datetime.now()} -- SCENARIO {args.id_scenario} for QOS {qos}!")
+        scenario_4_costs = {}
+        for model in pred_workloads:
+            demands = pred_workloads[model]
+            tot_costs = 0
+            new_gpus_status = df_gpus_status.copy()
+            for i, demand in enumerate(demands):
+                cost_i, new_gpus_status = allocate_resources_4(demand, new_gpus_status, i)
+                tot_costs += cost_i
+            scenario_4_costs[model] = np.sum(tot_costs)
+            model_column.append(model)
+            energy_costs.append(scenario_4_costs[model])
+            qos_column.append(qos)
+            print(f"Cost for {model}: {scenario_4_costs[model]}")
+            print(f"{datetime.datetime.now()} -- Done with model {model}!")
+        print(f"{datetime.datetime.now()} -- END!")
+
+    d = {"qos": qos_column,
+         "model": model_column,
+         "energy_cost": energy_costs}
+
+    df_results = pd.DataFrame(data=d)
+    df_results.to_csv("scenario_4_results.csv", index=False)
+
+
+def run_scenario_11(args, results: dict):
+
+    model_column = []
+    energy_costs = []
+    qos_column = []
+    for qos in QOS_LEVELS:
+        # load results
+        if args.debug_mode:
+            pred_workloads = {"oracle": results["HBNN"]["true_n_gpu"].values}
+        else:
+            pred_workloads = {"oracle": results["HBNN"]["true_n_gpu"].values,
+                              "naive": [AVG_GPU_SPECS[0]
+                                        for i in range(len(results["HBNN"]))],  # max possible workload
+                              "HBNN": results["HBNN"][f"pred_n_gpu_{qos}"].values,
+                              "MCD": results["MCD"][f"pred_n_gpu_{qos}"].values,
+                              "HBNN++": results["HBNN++"][f"pred_n_gpu_{qos}"].values,
+                              "LSTMQ": results["LSTMQ"][f"pred_n_gpu_{qos}"].values,
+                              "LSTM": results["LSTM"][f"pred_n_gpu_{qos}"].values,
+                              "PROPHET": results["PROPHET"][f"pred_n_gpu_{qos}"].values,
+                              }
+
+        print(f"{datetime.datetime.now()} -- SCENARIO {args.id_scenario} for QOS {qos}!")
+        scenario_11_costs = {}
+        cost_on_5_mins = 1
+        service_level = float(int(qos)/100)
+        for model in pred_workloads:
+            true_demands = pred_workloads["oracle"]
+            demands = pred_workloads[model]
+
+            tot_costs = 0
+            # calculate extra cost for under and over provisioning
+            for true_demand, pred_demand in zip(true_demands, demands):
+                # tot_costs += max(true_demand*service_level - pred_demand, 0) * cost_on_5_mins*1.2
+                # tot_costs += max(pred_demand - true_demand*service_level, 0) * cost_on_5_mins*0.2
+
+                tot_costs += max(true_demand - pred_demand, 0) * cost_on_5_mins*1.2
+                tot_costs += max(pred_demand - true_demand, 0) * cost_on_5_mins*0.2
+
+            scenario_11_costs[model] = tot_costs
+            model_column.append(model)
+            energy_costs.append(scenario_11_costs[model])
+            qos_column.append(qos)
+            print(f"Cost for {model}: {scenario_11_costs[model]}")
+            print(f"{datetime.datetime.now()} -- Done with model {model}!")
+        print(f"{datetime.datetime.now()} -- END!")
+
+        d = {"qos": qos_column,
+             "model": model_column,
+             "energy_cost": energy_costs}
+
+        df_results = pd.DataFrame(data=d)
+        df_results.to_csv("scenario_11_results.csv", index=False)
+
+
 def allocate_resources_22(w_demand: float,
                           real_demand: float,
                           df_allocation: pd.DataFrame,
@@ -350,11 +682,23 @@ def main(args):
 
     results = load_results(args.data_folder)
 
+    if args.id_scenario == "2":
+        print("RUNNING SCENARIO 2")
+        run_scenario_2(args, results)
+    if args.id_scenario == "3":
+        print("RUNNING SCENARIO 3")
+        run_scenario_3(args, results)
+    if args.id_scenario == "4":
+        print("RUNNING SCENARIO 4")
+        run_scenario_4(args, results)
+    if args.id_scenario == "11":
+        print("RUNNING SCENARIO 11")
+        run_scenario_11(args, results)
     if args.id_scenario == "22":
-        print("RUNNING SCENARIO PERSISTENCE MODE=OFF")
+        print("RUNNING SCENARIO 22")
         run_scenario_22(args, results)
     if args.id_scenario == "33":
-        print("RUNNING SCENARIO PERSISTENCE MODE=ON")
+        print("RUNNING SCENARIO 33")
         run_scenario_33(args, results)
 
 
